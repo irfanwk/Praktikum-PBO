b4c8e50277f8ad5ca1f05d01c620beff33eaaff2
irfanwk <irfan.wk.2705@gmail.com>
2025-04-02 12:26:22 +0800

    THT Meet 5

--- W5/105223005-Muhammad Irfan Wira Kusuma-THT-5/src/librarysystem/Perpustakaan.java
+++ W5/105223005-Muhammad Irfan Wira Kusuma-THT-5/src/librarysystem/Perpustakaan.java
@@ -6,7 +6,7 @@ public class Perpustakaan {
 
     public Perpustakaan(){}
 
-    public void tambahBuku(Buku buku) {
+    public void tambahBuku(Buku buku){
         if(jumlahBuku<koleksiBuku.length){
             koleksiBuku[jumlahBuku++]=buku;
             System.out.println("Buku "+buku.getJudul()+" ditambahkan ke perpustakaan");
@@ -15,12 +15,16 @@ public class Perpustakaan {
         }
     }
 
-    public void tampilkanBuku() {
+    public void tampilkanBuku(){
         System.out.println("=== Daftar Buku ===");
         for(int i=0; i<jumlahBuku; i++){
             Buku buku=koleksiBuku[i];
-            System.out.print("Judul: "+buku.getJudul()+", Penulis: "+buku.getPenulis()+", Tahun: "+buku.getTahunTerbit()+", Status: "+(buku.isStatusDipinjam() ? "Dipinjam":"Tersedia"));
-            if()
+            System.out.print("Judul: "+buku.getJudul()+", Penulis: "+buku.getPenulis()+", Tahun: "+buku.getTahunTerbit()+", Status: ");
+            if(buku.isStatusDipinjam()){
+                System.out.println("Disinjam");
+            }else{
+                System.out.println("Tersedia");
+            }
         }
     }
 }
